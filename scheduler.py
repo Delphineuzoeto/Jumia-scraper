412afb48e1408aab1ffe1c6b3681c17b988f61b7
Delphineuzoeto <delphineuzoeto@gmail.com>
2026-03-09 13:35:03 +0100

    add scheduler and multi-category support

--- scheduler.py
+++ scheduler.py
@@ -4,23 +4,17 @@ from scraper import run_scraper
 from sheets import push_to_sheets
 import pandas as pd
 
-# def job():
-#     print(('starting scheduled scrape...'))
-#      # Scrape
-#     data = run_scraper()
-#     #save to CSV
-#     df = pd.DataFrame
-#     df.to_csv("jumia_phonnes.csv", index=False)
-#     #push to sheets
-#     push_to_sheets(df, "https://docs.google.com/spreadsheets/d/1iXoPlCIDyo_s6VV7zXkPvZn4fuUS6Hks1lBdiQgWI5Q/edit")
-#     print("✅ Done! Sheet updated.")
-
 def job():
-    print("🔄 Starting scheduled scrape...")
-    df = run_scraper(urls=["https://www.jumia.com.ng/televisions/"])
-    df.to_csv("jumia_phones.csv", index=False)
+    print(('starting scheduled scrape...'))
+     # Scrape
+    data = run_scraper()
+    #save to CSV
+    df = pd.DataFrame
+    df.to_csv("jumia_phonnes.csv", index=False)
+    #push to sheets
     push_to_sheets(df, "https://docs.google.com/spreadsheets/d/1iXoPlCIDyo_s6VV7zXkPvZn4fuUS6Hks1lBdiQgWI5Q/edit")
     print("✅ Done! Sheet updated.")
+
 
 # run immediately on start
 job()
